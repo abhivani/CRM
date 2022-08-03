593eb7b6c882dacc8c4967256abb16cf3944008d
abhishekvani <abhishekvani80@gmail.com>
2022-08-03 14:32:05 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./component/landing/home/home.component";
+import {LoginComponent} from "./component/account/login/login.component";
+import {DashboardComponent} from "./component/dashboard/dashboard.component";
+import {ForgetPasswordComponent} from "./component/account/forget-password/forget-password.component";
+import {FooterComponent} from "./component/dashboard/footer/footer.component";
+import {HeaderComponent} from "./component/dashboard/header/header.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'forgetpassword', component:ForgetPasswordComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  exports: [RouterModule, FooterComponent, HeaderComponent]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/landing/home/home.component';
+import { LoginComponent } from './component/account/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import { ForgetPasswordComponent } from './component/account/forget-password/forget-password.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent,
+    ForgetPasswordComponent,
   ],
   imports: [
+    FormsModule,
+    MatFormFieldModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
